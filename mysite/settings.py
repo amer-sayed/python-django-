aca86098439c4e626ed11ff431fca9882fc70878
amer-sayed <amersayed@gmail.com>
2019-05-29 02:21:03 +0300

    2

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'amer.apps.AmerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # my site
-    'amer'
 ]
 
 MIDDLEWARE = [
